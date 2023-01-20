2a4cab3f279e471e9828b17953a21d58b3acae6a
Wills (Williams Gunawan) <38174320+WilliamsGunawan@users.noreply.github.com>
2023-01-20 22:52:20 +0700

    Update PetClinicIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -37,7 +37,6 @@ class PetClinicIntegrationTests {
 
 	@Test
 	public void testTitle() {
-		//System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.setBinary("/usr/bin/chromedriver");
 		options.addArguments("--headless");
